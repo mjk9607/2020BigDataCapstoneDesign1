c377a3ee7c7ae6f69ef14e6378f793ac7a04137d
mjk9607 <mjk9607@naver.com>
2020-07-19 16:41:44 +0900

    20-07-19 16:41 스트리밍 기능 구현 Commit

--- app.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask, render_template, url_for, request
-from flask_restful import reqparse, abort, Api, Resource
-import cv2
-
-
-c
--- camera.py
+++ camera.py
@@ -1,2 +1,18 @@
 import cv2
 
+ds_factor = 0.6
+
+class VideoCamera(object):
+  def __init__(self):
+    self.video = cv2.VideoCapture(0)
+
+  def __del__(self):
+    self.video.release()
+
+  def get_frame(self):
+    ret, frame = self.video.read()
+    frame = cv2.resize(frame, None, fx = ds_factor, fy = ds_factor, interpolation = cv2.INTER_AREA)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    ret, jpeg = cv2.imencode('.jpg', frame)
+    return jpeg.tobytes()
