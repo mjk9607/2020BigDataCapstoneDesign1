53a0601488cca421e21fdacda50591a1d3de6a12
mjk9607 <mjk9607@naver.com>
2020-08-04 23:47:15 +0900

    live, gallery 버튼 삭제 및 레이아웃 수정

--- cameratest.py
+++ cameratest.py
@@ -4,9 +4,9 @@ import pyautogui
 
 class VideoCamera(object):
   def get_frame(self):
-    frame = pyautogui.screenshot(region = (0, 0, 500, 500))
+    frame = pyautogui.screenshot(region = (400, 400, 400, 400))
     img_frame = np.array(frame)
     img_frame = cv2.cvtColor(img_frame, cv2.COLOR_RGB2BGR)
 
     ret, jpeg = cv2.imencode('.jpeg', img_frame)
-    return jpeg
+    return jpeg.tobytes()
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, Response, jsonify
-from camera import VideoCamera
+from cameratest import VideoCamera
 
 app = Flask(__name__)
 camera = VideoCamera()
@@ -9,17 +9,25 @@ camera = VideoCamera()
 def index():
   return render_template('index.html')
 
-def generator(camera):
-  while True:
-    yield (b'--frame\r\n'b'Contents-Type: image/jpeg\r\n\r\n' + camera.get_frame() + b'\r\n\r\n')
-
 @app.route('/video_feed')
 def video_feed():
   return Response(generator(camera), mimetype = 'multipart/x-mixed-replace; boundary=frame')
 
+def generator(camera):
+  while True:
+    yield (b'--frame\r\n'b'Contents-Type: image/jpeg\r\n\r\n' + camera.get_frame() + b'\r\n\r\n')
+
 @app.route('/capture')
 def capture():
   return camera.get_frame()
+
+@app.route('/gallery')
+def gallery():
+  return render_template('gallery.html')
+
+@app.route('/live')
+def live():
+  return render_template('live.html')
 
 @app.route('/getsize.json')
 def getsize():
@@ -30,7 +38,7 @@ if __name__ == '__main__':
   aPort = '5000'
   aDebug = True
 
-  # try:
-  app.run(host = aHost, port = aPort, debug = aDebug)
-  # except:
-  #   print('Unable to open Port : ' + aPort)
+  try:
+    app.run(host = aHost, port = aPort, debug = aDebug)
+  except:
+    print('Unable to open Port : ' + aPort)
